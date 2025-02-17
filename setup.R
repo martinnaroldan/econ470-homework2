d3b9bfa3aadf053c24d2dc0bcff178f2cc16ff49
Martinna Roldan <martinnacamile@gmail.com>
2025-02-16 21:21:30 -0500

    Homework Questions

--- setup.R
+++ setup.R
@@ -1,9 +1,9 @@
 # setup.R - Load all required libraries
-required_packages <- c("tidyverse", "readr", "dplyr", "ggplot2")
+required_packages <- c("tidyverse", "ggbeeswarm", "scales")
 
 # Install missing packages automatically
 new_packages <- required_packages[!(required_packages %in% installed.packages()[, "Package"])]
-if(length(new_packages)) install.packages(new_packages)
+if(length(new_packages)) install.packages(new_packages, repos = "https://cran.r-project.org")
 
 # Load all packages
 lapply(required_packages, library, character.only = TRUE)
